ccd465bc5d2e07127f3346d31f381bf1f40b94d4
Daris <vo14onoksteam@gmail.com>
2022-01-14 22:54:40 +0600

    оптимизированная версия

--- code.js
+++ code.js
@@ -1,46 +1,32 @@
 
 const url ="https://fakestoreapi.com/products";
+
 async function fetchURL(){
    
-    try{
+    try {
         const response = await fetch(url)
         const data = await response.json()
-        console.log(data);
-        const type =typeof(data)
-        console.log(type);
         data.forEach(function (elem, index, data){
-           
-        if (elem.category!=undefined) {
-            let div = document.createElement('div');
-            div.className = "category";
-            div.innerHTML =elem.category
-            document.body.append(div);
-
+        console.log(elem);
+        function renderElement(type) {
+            if (elem[type] != undefined) {
+                let div = document.createElement('div');
+                div.className = type;
+                div.innerHTML = elem[type];
+                document.body.append(div);
+            }
         }
-        if (elem.title!=undefined) {
-            let div = document.createElement('div');
-            div.className = "title";
-            div.innerHTML =elem.title
-            document.body.append(div);
-
-        }
-        if (elem.image!=undefined) {
+        renderElement('category');
+        renderElement('title');
+        if (elem.image != undefined) {
             let div = document.createElement('div');
             div.className = "image";
             const divURL = elem.image;
             div.innerHTML =`"<img src=${divURL}>"`;
-        
             document.body.append(div);
-
         }
-        if (elem.description!=undefined) {
-            let div = document.createElement('div');
-            div.className = "description";
-            div.innerHTML =elem.description
-            document.body.append(div);
-
-        }
-        if (elem.rating!=undefined) {
+        renderElement('description');
+        if (elem.rating != undefined) {
             let div = document.createElement('div');
             div.className = "rating";
              let Ratingstr =JSON.stringify(elem.rating)
@@ -48,14 +34,13 @@ async function fetchURL(){
              var y = '}';
             var rExp = new RegExp(x, "g");
             var rExpy = new RegExp(y, "g");
-            Ratingstr =Ratingstr.replace(rExp, '');
-            div.innerHTML=Ratingstr.replace(rExpy, '');
+            Ratingstr = Ratingstr.replace(rExp, '');
+            div.innerHTML= Ratingstr.replace(rExpy, '');
             document.body.append(div);
-
         }
         })
     }
-    catch(error){
+    catch(error) {
         console.error(error)
     }
 }
